6441c81175a6689e1454bfb8b3fb21a50eae7789
LukeBucher <lukebucherwork@gmail.com>
2022-12-06 01:57:18 -0500

    bug

--- main.py
+++ main.py
@@ -184,6 +184,7 @@ def main():
     #game.game_loop()
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    state = GPIO.input(23)
     while state:
         state = GPIO.input(23)
         print("wait")
