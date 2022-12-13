2d4dd9bed7d0d699e57d062621a47d21d4ba5a54
LuekBucher <lukebucherwork@gmail.com>
2022-12-13 14:39:46 -0500

    bug

--- main.py
+++ main.py
@@ -75,10 +75,9 @@ class Stacker_Game:
             self.end_game()
 
     def board_update(self):  # Update the current game state of the internal board
-
-
         def piece_removal():  # Remove parts of the game piece that are not supported by pieces underneath
             cur_y, cur_x = self.active_game_object.y_pos, self.active_game_object.x_pos
+            print(cur_y)
             if cur_y == 14:
                 return
             for tiles in range(self.active_game_object.length):
